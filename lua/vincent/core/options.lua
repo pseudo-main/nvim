4ffd6644ad200e72c4598ae21d58c431a9375a8f
pseudo-main <pseudo.main.git@gmail.com>
2024-12-04 09:38:48 +0100

    feature: add python indentation

--- lua/vincent/core/options.lua
+++ lua/vincent/core/options.lua
@@ -11,11 +11,22 @@ opt.signcolumn = "yes"
 opt.termguicolors = true
 
 -- [[ Formatting ]]
+-- Indentation C-like
 opt.tabstop = 2
 opt.shiftwidth = 2
 opt.expandtab = true
-opt.autoindent = true
-opt.smartindent = true
+opt.autoindent = false
+opt.smartindent = false
+
+-- Indentation Python
+vim.g.python_indent = {
+  open_paren = "shiftwidth()",
+  nested_paren = "shiftwidth()",
+  continue = "shiftwidth()",
+  closed_paren_align_last_line = false,
+}
+
+-- Text wrapping
 opt.wrap = false
 
 -- [[ Search ]]
