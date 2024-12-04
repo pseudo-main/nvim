ae62178611fd9f0ec3b1145fe0ef7ec97901ba32
pseudo-main <pseudo.main.git@gmail.com>
2024-12-04 09:35:05 +0100

    feature: add keymaps for centering, registers, unbinds

--- lua/vincent/core/keymaps.lua
+++ lua/vincent/core/keymaps.lua
@@ -5,3 +5,30 @@
 -- [[ Leader ]]
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+-- [[ General keymaps ]]
+local set = vim.keymap.set
+
+-- Center the cursor
+set("n", "<C-d>", "<C-d>zz")
+set("n", "<C-u>", "<C-u>zz")
+set("n", "n", "nzzzv")
+set("n", "N", "Nzzzv")
+set("n", "J", "mzJ`z")
+
+-- Keep register
+set("x", "<leader>p", [["_dP]])
+set({ "n", "v" }, "<leader>d", [["_d]])
+
+-- System register
+set({ "n", "v" }, "<leader>y", [["+y]])
+set("n", "<leader>Y", [["+Y]])
+set({ "n", "v" }, "<leader>p", [["+p]])
+set("n", "<leader>P", [["+P]])
+
+-- Unbind
+set("n", "Q", "<nop>")
+set("n", "<Up>", "<nop>")
+set("n", "<Right>", "<nop>")
+set("n", "<Down>", "<nop>")
+set("n", "<Left>", "<nop>")
