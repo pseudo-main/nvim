4caf4757348a40954093042da5c04e0ebd76b2c3
pseudo-main <pseudo.main.git@gmail.com>
2024-12-04 09:40:20 +0100

    feature: add undo file

--- lua/vincent/core/options.lua
+++ lua/vincent/core/options.lua
@@ -35,8 +35,10 @@ opt.smartcase = true
 opt.cursorline = true
 
 -- [[ Clipboard ]]
-opt.clipboard:append("unnamedplus")
 opt.swapfile = false
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
 
 -- [[ Splits ]]
 opt.splitright = true
