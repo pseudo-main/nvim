ee1850957b215aa4e91fcdcd6858f9d3d4a41670
pseudo-main <pseudo.main.git@gmail.com>
2025-04-19 16:52:50 +0200

    chore: clean up comments

--- lua/vincent/plugins/nvim-lspconfig.lua
+++ lua/vincent/plugins/nvim-lspconfig.lua
@@ -47,15 +47,13 @@ local keymaps = {
 	},
 }
 
--- [[ Plugins lspconfig, mason, mason-lspconfig, mason-tool-installer, cmp-nvim-lsp ]]
+-- [[ Plugin configuration ]]
 return {
 	"neovim/nvim-lspconfig",
 
 	dependencies = {
 		{
 			"folke/lazydev.nvim",
-
-			-- Recommended settings from lazydev documentation
 			ft = "lua",
 			opts = {
 				library = {
@@ -69,14 +67,13 @@ return {
 			lazy = true,
 		},
 
-		"williamboman/mason.nvim", -- External editor tooling integration manager (LSPs, DAPs, linting, formatters)
-		"williamboman/mason-lspconfig.nvim", -- Simplify integration between nvim-lspconfig and mason
-		"WhoIsSethDaniel/mason-tool-installer.nvim", -- External editor tooling installation manager
-		"hrsh7th/cmp-nvim-lsp", -- Extra capabilities provided by nvim-cmp
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		"hrsh7th/cmp-nvim-lsp",
 	},
 
 	config = function()
-		-- Run this function when LSP attaches to buffer
 		vim.api.nvim_create_autocmd("LSPAttach", {
 			group = vim.api.nvim_create_augroup("user-lsp-config", { clear = true }),
 
@@ -85,7 +82,6 @@ return {
 					utils.map_buf(event.buf, keymap.mode, keymap.lhs, keymap.rhs, keymap.opts)
 				end
 
-				-- Hover highlight
 				local client = vim.lsp.get_client_by_id(event.data.client_id)
 				if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
 					local highlight_augroup = vim.api.nvim_create_augroup("user-lsp-hover-highlight", { clear = false })
@@ -113,11 +109,9 @@ return {
 			end,
 		})
 
-		-- Required to broadcast additional capabilities to LSP servers
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
 		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-		-- [[ LSP servers configurations ]]
 		local servers = {
 			lua_ls = {
 				settings = {
@@ -138,15 +132,12 @@ return {
 			pyright = {},
 		}
 
-		-- [[ Tool installations ]]
-		-- Add additional stylers to install
 		local ensure_installed = vim.tbl_keys(servers)
 		vim.list_extend(ensure_installed, {
 			"stylua",
 			"ruff",
 		})
 
-		-- Install all tools
 		require("mason").setup()
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 		require("mason-lspconfig").setup({
