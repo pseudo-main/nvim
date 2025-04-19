dd61e84c03307186f43bbb1fc68be3a2f3499dc5
pseudo-main <pseudo.main.git@gmail.com>
2025-04-19 09:42:49 +0200

    refactor: extract keymaps to top of the file

--- lua/vincent/plugins/gitsigns.lua
+++ lua/vincent/plugins/gitsigns.lua
@@ -1,68 +1,70 @@
--- [[ Plugin gitsigns ]]
---  Settings have mostly been copied from the Github documentation.
+local utils = require("vincent.core.utils")
+
+-- [[ Keymaps ]]
+local function set_keymaps(bufnr)
+	local gitsigns = require("gitsigns")
+
+	-- Navigation
+	utils.map_buf(bufnr, "n", "]c", function()
+		if vim.wo.diff then
+			vim.cmd.normal({ "]c", bang = true })
+		else
+			gitsigns.nav_hunk("next")
+		end
+	end, { desc = "Next git hunk" })
+
+	utils.map_buf(bufnr, "n", "[c", function()
+		if vim.wo.diff then
+			vim.cmd.normal({ "[c", bang = true })
+		else
+			gitsigns.nav_hunk("prev")
+		end
+	end, { desc = "Previous git hunk" })
+
+	-- Hunk
+	utils.map_buf(bufnr, "n", "<leader>hs", gitsigns.stage_hunk, { desc = "Stage hunk" })
+	utils.map_buf(bufnr, "n", "<leader>hr", gitsigns.reset_hunk, { desc = "Reset hunk" })
+	utils.map_buf(bufnr, "n", "<leader>hu", gitsigns.undo_stage_hunk, { desc = "Undo stage hunk" })
+	utils.map_buf(bufnr, "n", "<leader>hp", gitsigns.preview_hunk, { desc = "Preview hunk" })
+
+	utils.map_buf(bufnr, "v", "<leader>hs", function()
+		gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+	end, { desc = "Stage hunk (visual)" })
+
+	utils.map_buf(bufnr, "v", "<leader>hr", function()
+		gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+	end, { desc = "Reset hunk (visual)" })
+
+	-- Buffer
+	utils.map_buf(bufnr, "n", "<leader>hS", gitsigns.stage_buffer, { desc = "Stage buffer" })
+	utils.map_buf(bufnr, "n", "<leader>hR", gitsigns.reset_buffer, { desc = "Reset buffer" })
+
+	-- Blame
+	utils.map_buf(bufnr, "n", "<leader>hb", function()
+		gitsigns.blame_line({ full = true })
+	end, { desc = "Blame line (full)" })
+
+	utils.map_buf(bufnr, "n", "<leader>tb", gitsigns.toggle_current_line_blame, { desc = "Toggle line blame" })
+
+	-- Diff
+	utils.map_buf(bufnr, "n", "<leader>hd", gitsigns.diffthis, { desc = "Diff against index" })
+
+	utils.map_buf(bufnr, "n", "<leader>hD", function()
+		gitsigns.diffthis("~")
+	end, { desc = "Diff against last commit" })
+
+	-- Deleted
+	utils.map_buf(bufnr, "n", "<leader>td", gitsigns.toggle_deleted, { desc = "Toggle deleted lines" })
+end
+
+-- [[ Plugin configuration ]]
 return {
 	"lewis6991/gitsigns.nvim",
 
 	config = function()
 		require("gitsigns").setup({
 			on_attach = function(bufnr)
-				local gitsigns = require("gitsigns")
-
-				local function map(mode, l, r, opts)
-					opts = opts or {}
-					opts.buffer = bufnr
-					vim.keymap.set(mode, l, r, opts)
-				end
-
-				-- [[ Keymaps ]]
-				-- Navigation
-				map("n", "]c", function()
-					if vim.wo.diff then
-						vim.cmd.normal({ "]c", bang = true })
-					else
-						gitsigns.nav_hunk("next")
-					end
-				end)
-
-				map("n", "[c", function()
-					if vim.wo.diff then
-						vim.cmd.normal({ "[c", bang = true })
-					else
-						gitsigns.nav_hunk("prev")
-					end
-				end)
-
-				-- Hunk
-				map("n", "<leader>hs", gitsigns.stage_hunk)
-				map("n", "<leader>hr", gitsigns.reset_hunk)
-				map("n", "<leader>hu", gitsigns.undo_stage_hunk)
-				map("n", "<leader>hp", gitsigns.preview_hunk)
-
-				map("v", "<leader>hs", function()
-					gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-				end)
-				map("v", "<leader>hr", function()
-					gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-				end)
-
-				-- Buffer
-				map("n", "<leader>hS", gitsigns.stage_buffer)
-				map("n", "<leader>hR", gitsigns.reset_buffer)
-
-				-- Blame
-				map("n", "<leader>hb", function()
-					gitsigns.blame_line({ full = true })
-				end)
-				map("n", "<leader>tb", gitsigns.toggle_current_line_blame)
-
-				-- Diff
-				map("n", "<leader>hd", gitsigns.diffthis)
-				map("n", "<leader>hD", function()
-					gitsigns.diffthis("~")
-				end)
-
-				-- Deleted
-				map("n", "<leader>td", gitsigns.toggle_deleted)
+				set_keymaps(bufnr)
 			end,
 		})
 	end,
