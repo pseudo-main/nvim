5ee5f5a63d8e4bb57f0ef75db9cbda7aa536f953
pseudo-main <pseudo.main.git@gmail.com>
2025-10-16 13:10:09 +0200

    feat: remove colorcolumn in toggleterm

--- lua/vincent/plugins/toggleterm.lua
+++ lua/vincent/plugins/toggleterm.lua
@@ -9,6 +9,13 @@ return {
 			start_in_insert = true,
 			close_on_exit = false,
 			open_mapping = [[<C-\>]],
+
+			vim.api.nvim_create_autocmd("TermOpen", {
+				pattern = "*",
+				callback = function()
+					vim.opt_local.colorcolumn = ""
+				end,
+			}),
 		})
 	end,
 }
