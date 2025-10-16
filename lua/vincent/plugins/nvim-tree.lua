f547e17c1c82588af030a58e41d334a33218c985
pseudo-main <pseudo.main.git@gmail.com>
2025-10-16 13:10:52 +0200

    feat: add keybind to toggle gitignore filtering

--- lua/vincent/plugins/nvim-tree.lua
+++ lua/vincent/plugins/nvim-tree.lua
@@ -1,10 +1,39 @@
 local utils = require("vincent.core.utils")
 
+-- [[ Functions ]]
+-- Toggle git.ignore setting dynamically
+local git_ignore = true
+function ToggleNvimTreeGitIgnore()
+	local view = require("nvim-tree.view")
+	local was_open = view.is_visible() -- Check if the tree is open
+
+	git_ignore = not git_ignore
+
+	require("nvim-tree").setup({
+		git = {
+			enable = true,
+			ignore = git_ignore,
+		},
+	})
+
+	if was_open then
+		vim.cmd("NvimTreeOpen") -- Reopen the tree if it was open
+	end
+
+	vim.cmd("NvimTreeRefresh")
+
+	vim.notify(
+		"Git ignore is now " .. (git_ignore and "ON (hiding ignored files)" or "OFF (showing ignored files)"),
+		vim.log.levels.INFO
+	)
+end
+
 -- [[ Keymaps ]]
 utils.map("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
 utils.map("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" })
 utils.map("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
 utils.map("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
+utils.map("n", "<leader>gi", ToggleNvimTreeGitIgnore, { desc = "Toggle Git ignore filtering in NvimTree" })
 
 -- [[ Plugin configuration ]]
 return {
@@ -38,12 +67,11 @@ return {
 				},
 			},
 
-			filters = {
-				custom = { ".DS_Store" },
-			},
+			filters = {},
 
 			git = {
-				ignore = false,
+				enable = true,
+				ignore = git_ignore,
 			},
 		})
 	end,
